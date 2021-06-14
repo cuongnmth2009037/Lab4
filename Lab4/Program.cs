aab7447663e95aa73065bc2e06a10a6ee7ba428d
t2009m1cuong <cuongnmth2009037@fpt.edu.vn>
2021-06-14 14:40:27 +0700

    up file Lab4

---
+++ Lab4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lab4
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person person = new Person("Cuong", "0964615565", "cuong@gmail.com");
+            Console.WriteLine(person.ToString());
+        }
+    }
+}
---
+++ Lab4/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab4
+{
+    public class Student : Person
+    {
+        public int Program { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name :{Name}, Phone :{Phone}, Email :{Email}, Program : {Program}";
+        }
+            
+    }
+}
